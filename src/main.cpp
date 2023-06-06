78798f65235a5005193af8346fd4ee62a81eb426
Nicolás Concha Vargas <niccy266@gmail.com>
2023-06-06 19:24:53 -0400

    Button in a Window

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,19 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QPushButton button;
-    button.setText("My text");
-    button.setToolTip("A tooltip");
+    QWidget window;
+    window.setFixedSize(100, 50);
+
+    QPushButton *button = new QPushButton("Hello", &window);
+
+    //button.setText("My text");
+    button->setToolTip("A tooltip");
+    button->setGeometry(10, 10, 80, 30);
 
     QFont font("Courier");
-    button.setFont(font);
+    button->setFont(font);
 
-    button.show();
+    window.show();
 
     return a.exec();
 }
